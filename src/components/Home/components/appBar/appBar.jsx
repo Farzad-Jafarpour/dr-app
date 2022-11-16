e51ae2e07d0564924dc9393be27be3dcf8895aef
unknown <farzad.jafarpour@gmail.com>
2022-11-16 20:13:22 +0330

    Doctor app initialized

---
+++ src/components/Home/components/appBar/appBar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CostumeAppBar = () => {
+  return <>Hi</>;
+};
+
+export default CostumeAppBar;
---
+++ src/components/Home/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CostumeAppBar from "./components/appBar/appBar";
+const HomePage = () => {
+  return (
+    <>
+      <CostumeAppBar />
+    </>
+  );
+};
+
+export default HomePage;
